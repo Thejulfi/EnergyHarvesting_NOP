b3ee61fc321880b6d217b0284918136b70eaf188
Thejulfi <40205050+Thejulfi@users.noreply.github.com>
2021-12-14 18:57:48 +0100

    functions add, better comments

--- UART_Hello_world_MSP430/main.c
+++ UART_Hello_world_MSP430/main.c
@@ -73,40 +73,45 @@
 //******************************************************************************
 #include <msp430.h>
 
+void init_UART(void){
+    WDTCTL = WDTPW | WDTHOLD;                 // Stop Watchdog
+
+    //******************************************************************************
+    // configure P2.0 and P2.1 FOR UART
+    //******************************************************************************
+    P2SEL1 |= BIT0 | BIT1;
+    P2SEL0 &= ~(BIT0 | BIT1);
+    // Disable the GPIO power-on default high-impedance mode to activate
+    // previously configured port settings
+    PM5CTL0 &= ~LOCKLPM5;
+
+    //******************************************************************************
+    //  Startup clock system with ~1MHz
+    //******************************************************************************
+    CSCTL0_H = CSKEY >> 8;                    // Unlock clock registers
+    CSCTL1 = DCOFSEL_0 | DCORSEL;             // Set DCO to 1MHz
+    CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
+    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers
+    CSCTL0_H = 0;                             // Lock CS registers
+
+    UCA0CTLW0 &= UCSWRST;                     //Take eUSC0 out into SW reset with UCA0CTLW0 = 1
+    UCA0CTLW0 |= UCSSEL__SMCLK;               // CLK = SMCLK
+    // Baud Rate calculation
+    // 10000000/(115200) = 8.68
+    // Fractional portion = 0.68
+    // User's Guide Table 21-4: UCBRSx = 0xD600
+    UCA0BR0 = 8;                              // Prescaler : 8000000/115200
+    UCA0MCTLW |= 0xD600;                      // Modulation
+    UCA0CTLW0 &= ~UCSWRST;                    //Take eUSC0 out of SW reset with UCA0CTLW0 = 0
+}
+
 int main(void)
 {
-  WDTCTL = WDTPW | WDTHOLD;                 // Stop Watchdog
 
-  // configure P2.0 and P2.1
-  P2SEL1 |= BIT0 | BIT1;
-  P2SEL0 &= ~(BIT0 | BIT1);
-
-
-  // Disable the GPIO power-on default high-impedance mode to activate
-  // previously configured port settings
-  PM5CTL0 &= ~LOCKLPM5;
-
-  // Startup clock system with ~1MHz
-  CSCTL0_H = CSKEY >> 8;                    // Unlock clock registers
-  CSCTL1 = DCOFSEL_0 | DCORSEL;             // Set DCO to 1MHz
-  CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
-  CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers
-  CSCTL0_H = 0;                             // Lock CS registers
-
-  // Configure USCI_A0 for UART mode
-  UCA0CTLW0 &= UCSWRST;                     //Take eUSC0 out into SW reset with UCA0CTLW0 = 1
-
-  UCA0CTLW0 |= UCSSEL__SMCLK;               // CLK = SMCLK
-
-  // Baud Rate calculation
-  // 10000000/(115200) = 8.68
-  // Fractional portion = 0.68
-  // User's Guide Table 21-4: UCBRSx = 0xD600
-
-  UCA0BR0 = 8;                              // Prescaler : 8000000/115200
-  UCA0MCTLW |= 0xD600;                      // Modulation
-
-  UCA0CTLW0 &= ~UCSWRST;                    //Take eUSC0 out of SW reset with UCA0CTLW0 = 0
+  init_UART();
+  //******************************************************************************
+  // Main
+  //******************************************************************************
 
   char message[] = "hello world ";          //Message buffer
   int position;                             //message index
