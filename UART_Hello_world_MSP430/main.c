2b778b8da441dc1e401f6a05e43bea1db73dc573
Thejulfi <40205050+Thejulfi@users.noreply.github.com>
2021-12-23 18:04:55 +0100

    update UART
    trying to make I2C work part 1

--- UART_Hello_world_MSP430/main.c
+++ UART_Hello_world_MSP430/main.c
@@ -14,30 +14,46 @@
 
 #include <msp430.h>
 
-void init_UART(void){
-    WDTCTL = WDTPW | WDTHOLD;                 // Stop Watchdog
-
-    //******************************************************************************
-    // configure P2.0 and P2.1 FOR UART
-    //******************************************************************************
+//******************************************************************************
+// configure P2.0 and P2.1 FOR UART
+//******************************************************************************
+void initGPIO()
+{
     P2SEL1 |= BIT0 | BIT1;
     P2SEL0 &= ~(BIT0 | BIT1);
     // Disable the GPIO power-on default high-impedance mode to activate
     // previously configured port settings
     PM5CTL0 &= ~LOCKLPM5;
+}
 
-    //******************************************************************************
-    //  Startup clock system with ~1MHz
-    //******************************************************************************
+//******************************************************************************
+//  Startup clock system with ~1MHz
+//******************************************************************************
+void initClockTo1MHz()
+{
+    // Configure one FRAM waitstate as required by the device datasheet for MCLK
+    // operation beyond 8MHz _before_ configuring the clock system.
+    FRCTL0 = FRCTLPW | NWAITS_1;
+
     CSCTL0_H = CSKEY >> 8;                    // Unlock clock registers
     CSCTL1 = DCOFSEL_0 | DCORSEL;             // Set DCO to 1MHz
+        //DCOFSEL_0 = 1Mhz
+        //DCOFSEL_1 = 5.33Mhz
+        //DCOFSEL_2 = 7Mhz
+        //DCOFSEL_3 = 8Mhz
+        //DCOFSEL_4 = 16Mhz
+        //DCOFSEL_5 = 21Mhz
+        //DCOFSEL_6 = 24Mhz
     CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
     CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers
-    CSCTL0_H = 0;                             // Lock CS registers
+    CSCTL0_H = 0;                             // Lock CS registers                       // Lock CS registerss
+}
 
-    //******************************************************************************
-    //  Setup UCA0 in UART mode
-    //******************************************************************************
+//******************************************************************************
+//  Setup UCA0 in UART mode
+//******************************************************************************
+void init_UART(void){
+    WDTCTL = WDTPW | WDTHOLD;                 // Stop Watchdog
     UCA0CTLW0 &= UCSWRST;                     //Take eUSC0 out into SW reset with UCA0CTLW0 = 1
     UCA0CTLW0 |= UCSSEL__SMCLK;               // CLK = SMCLK
     // Baud Rate calculation
@@ -49,27 +65,37 @@ void init_UART(void){
     UCA0CTLW0 &= ~UCSWRST;                    //Take eUSC0 out of SW reset with UCA0CTLW0 = 0
 }
 
+void UARTprint(char mes[]){
+
+
+
+    int position;
+
+    for(position = 0; position<sizeof(mes);position++){
+        UCA0TXBUF = mes[position];
+        __delay_cycles(100);
+    }
+}
+
 int main(void)
 {
-
+  //Various initialisations
+  initClockTo1MHz();
+  initGPIO();
   init_UART();
+
   //******************************************************************************
   // Main
   //******************************************************************************
-
-  char message[] = "hello world ";          //Message buffer
-  int position;                             //message index
-
+  //char message[] = ;          //Message buffer
 
   while(1){
-      for(position = 0; position<sizeof(message);position++){
-          UCA0TXBUF = message[position];
-          __delay_cycles(100);
-      }
+      char test[] = "12";
+
+      UARTprint(test);
       __delay_cycles(3000000);
 
 
   }
-
-  __no_operation(); // For debugger
 }
+
