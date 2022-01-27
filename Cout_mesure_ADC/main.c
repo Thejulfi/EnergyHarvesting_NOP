83a9d177d74a53f123ecb8d05a8be0c7266f05c5
TristanPicot <tristanpicot@hotmail.fr>
2022-01-27 14:21:15 +0100

    Ajout code cout ADC
    programme qui mesure le cout de 100 mesures toutes les secondes sur l'ADC

--- Cout_mesure_ADC/main.c
+++ Cout_mesure_ADC/main.c
@@ -5,11 +5,15 @@
 int ADC_value;
 float Current;
 float Voltage;
-unsigned int k = 0;
+unsigned int k = 0; //Variable for the position in the FRAM
+int i = 0; //Count the number of measure for ADC
 
 void initADC (); //Initialize the ADC
 void read_adc ();   //Read the value on the ADC and convert into current
-
+void storeFRAM (); //Store current and voltage into FRAM
+void init_timer_interrupt();
+void initClockTo16MHz();
+void initBoard ();
 
 
 //Command for write into the FRAM with a array of size WRITE_SIZE (1000)
@@ -40,71 +44,115 @@ unsigned long __attribute__((persistent)) FRAM_write[WRITE_SIZE] = {0};
        switch (__even_in_range(ADC12IV, ADC12IV_ADC12RDYIFG))
        {
        case ADC12IV_ADC12IFG0: // ADC12MEM0 Interrupt
-       read_adc (); //Read value on ADC
+
+           read_adc (); //Read value on ADC
+
+           //storeFRAM(); //Store the data into the FRAM
 
        __bic_SR_register_on_exit(LPM0_bits | GIE); // Exit CPU, clear interrupts
        break;
        default: break;
        }
+       //Realise 100 measure in the ADC
+       if (i <100){
+       i = i+1;
+       ADC12CTL0 |= ADC12ENC | ADC12SC; //Trigger the interrupt for ADC
+       }
       }
 
+  //******************************************************************************
+  // Timer interrupt *************************************************************
+  //******************************************************************************
 
-#pragma vector = PORT1_VECTOR
-  __interrupt void ISR_PORT1_S1(void)
+  #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+  #pragma vector=TIMER0_B1_VECTOR
+  __interrupt void TIMER0_B1_ISR(void)
+  #elif defined(__GNUC__)
+  void __attribute__ ((interrupt(TIMER0_B1_VECTOR))) TIMER0_B1_ISR (void)
+  #else
+  #error Compiler not supported!
+  #endif
   {
-      P1IFG &= ~BIT1;
-
-      ADC12CTL0 |= ADC12ENC | ADC12SC; // Start sampling/conversion
-      //__bis_SR_register(LPM0_bits | GIE); // LPM0, ADC12_ISR will force exit
+    switch(__even_in_range(TB0IV,TB0IV_TBIFG))
+    {
+      case TB0IV_NONE:   break;               // No interrupt
+      case TB0IV_TBCCR1: break;               // CCR1 not used
+      case TB0IV_TBCCR2: break;               // CCR2 not used
+      case TB0IV_TBCCR3: break;               // CCR3 not used
+      case TB0IV_TBCCR4: break;               // CCR4 not used
+      case TB0IV_TBCCR5: break;               // CCR5 not used
+      case TB0IV_TBCCR6: break;               // CCR6 not used
+      case TB0IV_TBIFG:                       // overflow
+          i = 0; //Reset the number of measure by the ADC
+          ADC12CTL0 |= ADC12ENC | ADC12SC; //Trigger the interrupt for ADC
+        break;
+      default: break;
+    }
   }
 
+/*************************************MAIN*******************************************/
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    initBoard (); //Init I/O of the system
+    initADC (); //Init the ADC
 
-    initADC ();
+    initClockTo16MHz();
+    init_timer_interrupt();                     // Initialization of timer interrupt
 
-    P1DIR &= ~BIT1;
-    P1REN |= BIT1;
-    P1OUT |= BIT1;
-    P1IES |= BIT1;
-
-    PM5CTL0 &= ~LOCKLPM5;
-
-    P1IFG &= ~BIT1;
-    P1IE |= BIT1;
-    __enable_interrupt();
-
+    ADC12CTL0 |= ADC12ENC | ADC12SC; //Trigger the first interrupt for ADC
     while (1)
     {
-        //ADC12CTL0 |= ADC12ENC | ADC12SC; // Start sampling/conversion
-        __bis_SR_register(LPM3_bits | GIE);         // Enter LPM0, enable interrupts
+        __bis_SR_register(LPM3_bits | GIE);         // Enter LPM3, enable interrupts
         __no_operation();                           // For debugger
     }
 }
 
+
+/**************FONCTIONS***************************/
 void read_adc (){
     ADC_value = ADC12MEM0; // Save MEM0
 
        //Overflow of Voltage when the real voltage is higher than 0.5V
-       Voltage = ((ADC_value)*1000)/1340; //Voltage * 1000 (result in mV)
+       Voltage = ((ADC_value)*100)/1340; //Voltage * 1000 (result in mV)
        Current = (Voltage/25)*1000; //Current *0 100 (in µA)
 
 
-        if(k<=1000){
-       //Write the value of the current to the adress k
-       FRAM_write[k] = Current;
-       //k is incremented to go to the next adress of the FRAM
-       k = k+1;
-
-       //Write the value of the voltage to the adress k
-       FRAM_write[k] = Voltage;
-       //k is incremented to go to the next adress of the FRAM
-       k = k+3;
-       }
 }
 
+void storeFRAM(){
 
+       if(k<=1000){
+      //Write the value of the current to the adress k
+      FRAM_write[k] = Current;
+      //k is incremented to go to the next adress of the FRAM
+      k = k+1;
+
+      //Write the value of the voltage to the adress k
+      FRAM_write[k] = Voltage;
+      //k is incremented to go to the next adress of the FRAM
+      k = k+1;
+      }
+}
+
+void initBoard ( void )
+{
+    // Port Configuration
+    // stored in FRAM
+    P1OUT = 0x00 ;
+    P1DIR = 0xFF ;
+    P2OUT = 0x00 ;
+    P2DIR = 0xFF ;
+    P3OUT = 0x00 ;
+    P3DIR = 0xFF ;
+    P4OUT = 0x00 ;
+    P4DIR = 0xFF ;
+    PJOUT = 0x00 ;
+    PJSEL0 |= BIT4 | BIT5 ;
+    PJDIR = 0xFFFF ;
+
+    PM5CTL0 &= ~LOCKLPM5;
+}
 
 void initADC ()
 {
@@ -124,3 +172,29 @@ void initADC ()
 }
 
 
+void initClockTo16MHz()
+{
+    // Configure one FRAM waitstate as required by the device datasheet for MCLK
+    // operation beyond 8MHz _before_ configuring the clock system.
+    FRCTL0 = FRCTLPW | NWAITS_1;
+
+    // Clock System Setup
+    CSCTL0_H = CSKEY >> 8;                      // Unlock CS registers
+    CSCTL1 = DCORSEL | DCOFSEL_4;               // Set DCO to 16MHz
+    CSCTL2 = SELA__LFXTCLK | SELS__DCOCLK | SELM__DCOCLK; // Sources selection LFXTCLK for ACLK, DCOCLK for SMCLK and DCOCLK for MCLK.
+    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;       // Set all dividers
+
+    CSCTL0_H = 0;                               // Lock CS registerss
+}
+
+void init_timer_interrupt(void){
+    TB0CTL |= TBCLR;                            // Clock divider logic clear, always read as zero.
+    TB0CTL |= TBSSEL__ACLK;                     // Timer clock source select : SMCLK.
+    TB0CTL |= MC__CONTINOUS;                    // Mode control to continous mode : Timer counts up to the value set by CNTL.
+    TB0CTL |= ID__8;                            // No clock divider.
+    TB0CTL |= CNTL_1;                           // Counter size (12 bits)
+
+
+    TB0CTL |= TBIE;                             // Enable interruption
+    TB0CTL &= ~TBIFG;                           // Switch to interrupt pending - interrupt flag
+}
