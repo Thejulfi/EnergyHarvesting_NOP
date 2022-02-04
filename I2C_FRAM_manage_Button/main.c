f14e07c3c606e72ccc4f8196054300cca8287e19
Thejulfi <40205050+Thejulfi@users.noreply.github.com>
2022-02-04 11:13:41 +0100

    update comments

--- I2C_FRAM_manage_Button/main.c
+++ I2C_FRAM_manage_Button/main.c
@@ -61,8 +61,8 @@
 
 
 // The Slave ADdress (SAD) associated to the LIS3MDL is 00111x0b, whereas the x bit is modified by the SDO/SA1 pin in order to modify the device address.
-#define SLAVE_ADDR_1  0x1C // Slave address where x is at 0
-#define SLAVE_ADDR_2  0x1E // Slave address where x is at 1
+#define SLAVE_ADDR_1  0x1C              // Slave address where x is at 0
+#define SLAVE_ADDR_2  0x1E              // Slave address where x is at 1
 
 
 /* ST_REG_X_MASTER: numerical address of the first register to write in the slave.
@@ -72,19 +72,19 @@
  * TYPE_X_LENGTH: packets' length (in byte) to read or to write
  * */
 
-#define REG_RD_1_SLAVE      44 // OUT_Z_L register address to read (0x2C)
-#define REG_RD_2_SLAVE      45 // OUT_Z_H register address to read (0x2D)
+#define REG_RD_1_SLAVE      44          // OUT_Z_L register address to read (0x2C)
+#define REG_RD_2_SLAVE      45          // OUT_Z_H register address to read (0x2D)
 
 
-#define ST_REG_1_MASTER      160 // CTRL_REG1 register to write first (0x20) with msb at "1" to enable address incrementation (for next CTRL_REGs)
+#define ST_REG_1_MASTER      160        // CTRL_REG1 register to write first (0x20) with msb at "1" to enable address incrementation (for next CTRL_REGs)
 
 #define DefaultConfiguration_LENGTH   5 // Write into CTRL_REG1..CTRL_REG5
-#define Slave_RD_LENGTH   1 // reading one byte in OUT_X_L & OUT_X_H
+#define Slave_RD_LENGTH   1             // reading one byte in OUT_X_L & OUT_X_H
 
-#define MAX_BUFFER_SIZE     20 // Maximum buffer size in reception and transmission
+#define MAX_BUFFER_SIZE     20          // Maximum buffer size in reception and transmission
 
 
-#define WRITE_SIZE 1000 // FRAM size of 1000
+#define WRITE_SIZE 1000                 // FRAM size of 1000
 
 
 /* DefaultConfiguration: configuration for CTRL_REG1..CTRL_REG5 (these are numerical values, not hexadecimal values)
@@ -216,13 +216,13 @@ I2C_Mode I2C_Master_ReadReg(uint8_t dev_addr, uint8_t reg_addr, uint8_t count)
 
     /* Initialize slave address and interrupts */
     UCB0I2CSA = dev_addr;
-    UCB0IFG &= ~(UCTXIFG + UCRXIFG);       // Clear any pending interrupts
-    UCB0IE &= ~UCRXIE;                       // Disable RX interrupt
-    UCB0IE |= UCTXIE;                        // Enable TX interrupt
+    UCB0IFG &= ~(UCTXIFG + UCRXIFG);                // Clear any pending interrupts
+    UCB0IE &= ~UCRXIE;                              // Disable RX interrupt
+    UCB0IE |= UCTXIE;                               // Enable TX interrupt
 
     /* Start condition and sleep mode */
-    UCB0CTLW0 |= UCTR + UCTXSTT;             // I2C TX, start condition
-    __bis_SR_register(LPM0_bits + GIE);              // Enter LPM0 w/ interrupts
+    UCB0CTLW0 |= UCTR + UCTXSTT;                    // I2C TX, start condition
+    __bis_SR_register(LPM0_bits + GIE);             // Enter LPM0 w/ interrupts
 
     return MasterMode;
 
@@ -243,15 +243,15 @@ I2C_Mode I2C_Master_WriteReg(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_da
 
     /* Initialize slave address and interrupts */
     UCB0I2CSA = dev_addr;
-    UCB0IFG &= ~(UCTXIFG + UCRXIFG);       // Clear any pending interrupts
-    UCB0IE &= ~UCRXIE;                       // Disable RX interrupt
-    UCB0IE |= UCTXIE;                        // Enable TX interrupt
+    UCB0IFG &= ~(UCTXIFG + UCRXIFG;             // Clear any pending interrupts
+    UCB0IE &= ~UCRXIE;                          // Disable RX interrupt
+    UCB0IE |= UCTXIE;                           // Enable TX interrupt
 
     /* Start condition and sleep mode */
-    UCB0CTLW0 |= UCTR;             // I2C TX, start condition
+    UCB0CTLW0 |= UCTR;                          // I2C TX, start condition
     UCB0CTLW0 |= UCTXSTT;
 
-    __bis_SR_register(LPM0_bits + GIE);              // Enter LPM0 w/ interrupts
+    __bis_SR_register(LPM0_bits + GIE);         // Enter LPM0 w/ interrupts
 
     return MasterMode;
 }
@@ -339,7 +339,7 @@ void initI2C(uint8_t dev_addr)
     UCB0CTLW0 = UCSWRST;                      // Enable SW reset
     UCB0CTLW0 |= UCMODE_3 | UCMST | UCSSEL__SMCLK | UCSYNC; // I2C master mode, SMCLK
     UCB0BRW = 160;                            // fSCL = SMCLK/160 = ~100kHz
-    UCB0I2CSA = dev_addr;                   // Slave Address
+    UCB0I2CSA = dev_addr;                     // Slave Address
     UCB0CTLW0 &= ~UCSWRST;                    // Clear SW reset, resume operation
     UCB0IE |= UCNACKIE;
 
@@ -349,11 +349,11 @@ void initI2C(uint8_t dev_addr)
 
 
 void init_timer_interrupt(void){
-    TB0CTL |= TBCLR;
-    TB0CTL |= TBSSEL__ACLK;
-    TB0CTL |= MC__CONTINOUS;
-    TB0CTL |= ID__8;
-    TB0CTL |= CNTL_1;
+    TB0CTL |= TBCLR;                        // clear
+    TB0CTL |= TBSSEL__ACLK;                 // source clock selection
+    TB0CTL |= MC__CONTINOUS;                // continous mode control
+    TB0CTL |= ID__8;                        // input divider by 8
+    TB0CTL |= CNTL_1;                       // counter size (8 bits)
 
 }
 
@@ -364,9 +364,9 @@ void init_timer_interrupt(void){
 
 // toggling LED1.0 on/off
 void toggle_led(void){
-    P1OUT ^= BIT0; // toggle led
+    P1OUT ^= BIT0;                          // toggle led
 
-    TB0CTL &= ~TBIFG; //disable interrupt
+    TB0CTL &= ~TBIFG;                       // disable interrupt
 }
 
 
@@ -384,7 +384,8 @@ void measurement(void){
 
     count ++;
 
-    toggle_led(); // Toggle LED1.0 to show that a data has been written in the FRAM
+    // YOU CAN REMOVE IT IF YOU ONLY WANT TO MONITOR BATTERY WITHOUT LEDS
+    toggle_led();                          // Toggle LED1.0 to show that a data has been written in the FRAM
 }
 
 
@@ -475,10 +476,10 @@ void __attribute__ ((interrupt(USCI_B0_VECTOR))) USCI_B0_ISR (void)
         {
           UCB0IE &= ~UCRXIE;
           MasterMode = IDLE_MODE;
-          __bic_SR_register_on_exit(CPUOFF);      // Exit LPM0
+          __bic_SR_register_on_exit(CPUOFF);        // Exit LPM0
         }
         break;
-    case USCI_I2C_UCTXIFG0:                 // Vector 24: TXIFG0
+    case USCI_I2C_UCTXIFG0:                         // Vector 24: TXIFG0
         switch (MasterMode)
         {
           case TX_REG_ADDRESS_MODE:
@@ -490,16 +491,16 @@ void __attribute__ ((interrupt(USCI_B0_VECTOR))) USCI_B0_ISR (void)
               break;
 
           case SWITCH_TO_RX_MODE:
-              UCB0IE |= UCRXIE;              // Enable RX interrupt
-              UCB0IE &= ~UCTXIE;             // Disable TX interrupt
-              UCB0CTLW0 &= ~UCTR;            // Switch to receiver
-              MasterMode = RX_DATA_MODE;    // State state is to receive data
-              UCB0CTLW0 |= UCTXSTT;          // Send repeated start
+              UCB0IE |= UCRXIE;                     // Enable RX interrupt
+              UCB0IE &= ~UCTXIE;                    // Disable TX interrupt
+              UCB0CTLW0 &= ~UCTR;                   // Switch to receiver
+              MasterMode = RX_DATA_MODE;            // State state is to receive data
+              UCB0CTLW0 |= UCTXSTT;                 // Send repeated start
               if (RXByteCtr == 1)
               {
                   //Must send stop since this is the N-1 byte
                   while((UCB0CTLW0 & UCTXSTT));
-                  UCB0CTLW0 |= UCTXSTP;      // Send stop condition
+                  UCB0CTLW0 |= UCTXSTP;             // Send stop condition
               }
               break;
 
@@ -512,10 +513,10 @@ void __attribute__ ((interrupt(USCI_B0_VECTOR))) USCI_B0_ISR (void)
               else
               {
                   //Done with transmission
-                  UCB0CTLW0 |= UCTXSTP;     // Send stop condition
+                  UCB0CTLW0 |= UCTXSTP;             // Send stop condition
                   MasterMode = IDLE_MODE;
-                  UCB0IE &= ~UCTXIE;                       // disable TX interrupt
-                  __bic_SR_register_on_exit(CPUOFF);      // Exit LPM0
+                  UCB0IE &= ~UCTXIE;                // disable TX interrupt
+                  __bic_SR_register_on_exit(CPUOFF);// Exit LPM0
               }
               break;
 
@@ -589,12 +590,12 @@ void __attribute__ ((interrupt(TIMER0_B1_VECTOR))) TIMER0_B1_ISR (void)
 #pragma vector = PORT1_VECTOR
 __interrupt void ISR_PORT1_S1(void)
 {
-  P1IFG &= ~BIT1; // disable interruption flag on button 1.1
+  P1IFG &= ~BIT1;                           // disable interruption flag on button 1.1
 
-  TB0CTL ^= TBIE; //disable - enable interruption
-  TB0CTL &= ~TBIFG; // set interrupt flag
+  TB0CTL ^= TBIE;                           //disable - enable interruption
+  TB0CTL &= ~TBIFG;                         // set interrupt flag
 
-  P1OUT = 0xFE; // turn off LED at P1OUT
+  P1OUT = 0xFE;                             // turn off LED at P1OUT
 
 }
 
@@ -612,7 +613,7 @@ __interrupt void ISR_PORT4_S5(void)
       FRAM_write[j] = (float)0;
 
   }
-  // reset i value
-  i = 0;
+
+  i = 0;                                    // reset i value
 
 }
